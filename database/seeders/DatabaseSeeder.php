6fd5030b957c4c923c12040d7dac739782cd5f83
MoEmam203 <mostafa.emamdar4@gmail.com>
2022-06-01 16:33:21 +0200

    load seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Customers\Database\Seeders\CustomerSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CustomerSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
