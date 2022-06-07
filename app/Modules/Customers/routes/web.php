7f9bba836883ca63821eb07405d4c020a6d06fb8
MoEmam203 <mostafa.emamdar4@gmail.com>
2022-06-07 18:02:33 +0200

    last enhancement & Finish course

--- app/Modules/Customers/routes/web.php
+++ app/Modules/Customers/routes/web.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+$moduleName = strtolower(basename(dirname(__DIR__, 1)));
 
-Route::prefix(buildPrefix('customers'))->middleware('customer')->group(function(){
+Route::prefix(buildPrefix($moduleName))->middleware('customer')->group(function(){
     require 'backend.php';
 });
 
-Route::prefix(buildPrefix('customers','frontend'))->middleware('customer')->group(function(){
+Route::prefix(buildPrefix($moduleName,'frontend'))->middleware('customer')->group(function(){
     require 'frontend.php';
 });
 
